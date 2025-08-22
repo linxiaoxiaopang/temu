8977bea656fc66391a207ca84a09f6caa4bfa5dd
SmartLin <linguangyan@cnzhengding.com>
2025-08-22 10:42:07 +0800

    优化

--- request-header-plugin/background.js
+++ request-header-plugin/background.js
@@ -1,70 +1,43 @@
-let requests = []
-let maxRequests = 100 // 最大存储请求数
-
 // 监听请求发送事件，捕获请求头
 chrome.webRequest.onSendHeaders.addListener(
   (details) => {
     // 构建请求信息对象
-    const requestInfo = {
-      id: Date.now(),
-      url: details.url,
-      method: details.method,
-      time: new Date(details.timeStamp).toLocaleTimeString(),
-      headers: details.requestHeaders || []
-    }
     const headers = details.requestHeaders.reduce((prev, cur) => {
       prev[cur.name] = cur.value
       return prev
     }, {})
-    if (details.url === 'https://api.toaddit.com/toadditbusiness/productService/productCategory/list' && !headers.isZucstomHeader) {
+
+    if (!headers.isZucstomHeader && details.method != 'OPTIONS') {
       headers.isZucstomHeader = 1
-      fetch('https://api.toaddit.com/toadditbusiness/productService/productPrototype/list', {
+      fetch('http://localhost:3000/setHeaders', {
         method: 'post',
-        headers: headers,
+        headers: {
+          'Content-Type': 'application/json'
+        },
         body: JSON.stringify({
-          // orderItems: [{ asc: false, column: 'sort_number' }],
-          page: {
-            pageIndex: 1,
-            pageSize: 0
-          }
+          headers
         })
       }).then(res => {
         return res.json()
-      }).then(res => {
-        debugger
-      }).catch(err => {
-        debugger
       })
     }
-
-    // 添加到请求列表前端
-    requests.unshift(requestInfo)
-
-    // 限制请求数量，防止内存占用过大
-    if (requests.length > maxRequests) {
-      requests = requests.slice(0, maxRequests)
-    }
-
-    // 保存到本地存储
-    chrome.storage.local.set({ requests: requests })
   },
-  { urls: ["<all_urls>"] }, // 监听所有URL
+  { urls: getMatchUrls(['userInfo', 'getUserInfo']) }, // 监听所有URL
+  // { urls: ["<all_urls>"] }, // 监听所有URL
   ["requestHeaders", "extraHeaders"] // 需要获取的信息
 )
 
-// 处理来自popup的消息
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  switch (message.type) {
-    case "GET_REQUESTS":
-      sendResponse({ requests: requests })
-      break
-    case "CLEAR_REQUESTS":
-      requests = []
-      chrome.storage.local.set({ requests: [] })
-      sendResponse({ success: true })
-      break
-    default:
-      sendResponse({})
-  }
-})
 
+function getMatchUrls(apiNameList) {
+  const separatorCount = 8
+  if (!Array.isArray(apiNameList)) apiNameList = [apiNameList]
+  const tmpArr = []
+  apiNameList.map(apiName => {
+    const protocol = '*://'
+    for (let i = 1; i <= separatorCount; i++) {
+      const path = new Array(i).fill('*').join('/')
+      tmpArr.push(`${protocol}${path}/${apiName}`)
+    }
+  })
+  return tmpArr
+}
